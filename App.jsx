8b68e0d658b771d50704af9b77fd6b668c7101a1
Aneeb ur Rehman <aneeb.02@gmail.com>
2026-06-30 21:36:34 +0500

    create basic react app

---
+++ App.jsx
@@ -0,0 +1,15 @@
+// react component names always start with capital letter
+function MyButton() {
+    return (
+        <button>Press me</button>
+    )
+}
+
+export default function App() {
+    return (
+        <div>
+            <h1>Welcome</h1>
+            <MyButton/>
+        </div>
+    )
+}
