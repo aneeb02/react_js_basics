daae66b51aa08d2018a47b31fa45b435ee180a8e
Aneeb ur Rehman <aneeb.02@gmail.com>
2026-07-01 01:40:04 +0500

    shared state, lifting state up

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # react_js_basics
+What I have learnt so far:
+- React.js basics
+- useState
+- components
+- conditional rendering
--- react-playground/src/Products.jsx
+++ react-playground/src/Products.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import { useState } from "react";
 const products = [
-  { title: "Cabbage", id: 1, isFruit: false },
-  { title: "Garlic", id: 2, isFruit: false },
-  { title: "Apple", id: 3, isFruit: true },
+  { title: "Cabbage", id: 1, isFruit: false, color:'green' },
+  { title: "Garlic", id: 2, isFruit: false, color:'yellow' },
+  { title: "Apple", id: 3, isFruit: true, color:'red' },
 ];
 
 // const Products = () => {
@@ -29,23 +29,36 @@ const products = [
 
 // giving each item its own state
 
-function ProductItem({ product }) {
+function ProductItem({ product, position, onItemClick }) {
   const [count, setCount] = useState(0);
+  
+  function handleClick(){
+    setCount(count+1);
+    onItemClick();
+  }
   return (
-    <li onClick={() => setCount(count + 1)}>
-      {product.title} clicked {count} times
+    <li onClick={handleClick}>
+      #{position}: {product.title} clicked {count} times
     </li>
   );
 }
 
 function Products() {
+  const [clicks, setClicks] = useState(0);
   return (
-    <ul>
-      {products.map((product) => (
-        <ProductItem key={product.id} product={product} />
-      ))}
-    </ul>
+      <>
+        <p>Total Clicks: {clicks}</p>
+        <ul>
+          {products.map((product, index) => (
+            <ProductItem 
+              key={product.id} 
+              product={product} 
+              position={index+1} 
+              onItemClick={() => setClicks(clicks+1)} 
+            />
+          ))}
+        </ul>
+      </>
   );
 }
-
 export default Products;
