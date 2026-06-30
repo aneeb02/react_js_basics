c9f3c2484e9b83c8dea576034560e69c24e4f74b
Aneeb ur Rehman <aneeb.02@gmail.com>
2026-06-30 23:37:04 +0500

    give individual states to items

--- react-playground/src/Products.jsx
+++ react-playground/src/Products.jsx
@@ -1,37 +1,51 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 const products = [
-    { title: 'Cabbage', id: 1, isFruit: false },
-    { title: 'Garlic', id: 2, isFruit: false },
-    { title: 'Apple', id: 3, isFruit: true },
-  ];
+  { title: "Cabbage", id: 1, isFruit: false },
+  { title: "Garlic", id: 2, isFruit: false },
+  { title: "Apple", id: 3, isFruit: true },
+];
 
-const Products = () => {
+// const Products = () => {
+//   const [count, setCount] = useState(0);
 
-    const [count, setCount] = useState(0);
+//   function handleClick() {
+//     setCount(count + 1);
+//   }
 
+//   const listItems = products.map((product) => (
+//     <li
+//       key={product.id}
+//       style={{
+//         color: product.isFruit ? "red" : "yellow",
+//       }}
+//       onClick={handleClick}
+//     >
+//       {product.title} Clicked {count} times
+//     </li>
+//   ));
+//   return <ul>{listItems}</ul>;
+// };
 
-    function handleClick() {
-        setCount(count+1);
-    }
-  
-    const listItems = products.map(product =>
-        <li 
-            key={product.id}
-            style={{
-                color: product.isFruit ? 'red': 'yellow'
-            }}
-            onClick={handleClick}
-        >
-            {product.title} Clicked {count} times
-        </li>
-   )
-    return (
-        <ul>{listItems}</ul>
-  )
+// giving each item its own state
+
+function ProductItem({ product }) {
+  const [count, setCount] = useState(0);
+  return (
+    <li onClick={() => setCount(count + 1)}>
+      {product.title} clicked {count} times
+    </li>
+  );
 }
 
+function Products() {
+  return (
+    <ul>
+      {products.map((product) => (
+        <ProductItem key={product.id} product={product} />
+      ))}
+    </ul>
+  );
+}
 
-
-
-export default Products
+export default Products;
